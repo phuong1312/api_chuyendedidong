381fd6c062dbd2117a21aa2d1a1acfb777ee4626
phuong1312 <20211tt1151@mail.tdc.edu.vn>
2022-12-13 19:46:16 +0700

    test2

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,11 +9,12 @@ const router = require('express').Router();
 // //add user
 // router.post('/add', userController.addUser);
 //get all user
-router.get('/all', userController.getAllUser);
+// router.get('/all', userController.getAllUser);
 // //get one user by id
 // router.get('/getbyid/:id', userController.getUserById);
 // //delete user by id
 // router.delete('/delete/:id', userController.deleteUser);
 // //update user
 // router.put('/update/:id', userController.updateUserById);
+console.log("ok");
 module.exports = router;
