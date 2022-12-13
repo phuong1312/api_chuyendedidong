86e8a7342e3234652e7d3e417d7a10aa895317f6
phuong1312 <20211tt1151@mail.tdc.edu.vn>
2022-12-13 19:13:22 +0700

    up2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -6,76 +7,90 @@ const morgan = require("morgan");
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
-require("dotenv").config();
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("common"));
+// app.use(express.json());
 app.use(cors());
-app.use("./uploads", express.static(path.join(__dirname, "uploads")));
-app.set("view engine", "ejs");
+// app.use(morgan("common"));
+// app.use("./uploads", express.static(path.join(__dirname, "uploads")));
+// app.set("viewengine", "ejs");
+
+// var storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === "image/jpg" || file.mimetype === "image/png") {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+
+// var upload = multer({ storage: storage });
 
 // var dbConn = require("./src/config/config");
-const DBConnect = async () => {
-  try {
-      await mongoose.connect(process.env.DATABASE_URL, {
-          useUnifiedTopology: true,
-          useNewUrlParser: true
-      }, {server:{auto_reconnect:true}});
-  } catch (err) {
-      console.error(err);
-  }
-}; 
-DBConnect();
-const userRoute = require("./src/routes/user");
-const drinkRoute = require("./src/routes/drink");
-const categoryRoute = require("./src/routes/category");
-const roleRoute = require("./src/routes/role");
-const areaRoute = require("./src/routes/area");
-const tableRoute = require("./src/routes/table");
-const drinkOrderRoute = require("./src/routes/drinkOrder");
-const orderRoute = require("./src/routes/order");
-const authorization = require('./src/config/authtokenrequired');
+// const upload = require("./src/config/cloudinary.config");
 
-app.use("/api/user", userRoute);
-app.use("/api/role", roleRoute);
-app.use("/api", drinkRoute);
-app.use("/api", categoryRoute);
-app.use("/api", areaRoute);
-app.use("/api", tableRoute);
-app.use("/api", drinkOrderRoute);
-app.use("/api", orderRoute);
+// const userRoute = require("./src/routes/user");
+// const drinkRoute = require("./src/routes/drink");
+// const categoryRoute = require("./src/routes/category");
+// const roleRoute = require("./src/routes/role");
+// const areaRoute = require("./src/routes/area");
+// const tableRoute = require("./src/routes/table");
+// const drinkOrderRoute = require("./src/routes/drinkOrder");
+// const orderRoute = require("./src/routes/order");
+// const authorization = require('./src/config/authtokenrequired');
 
-mongoose.connection.once("open", () => {
-  console.log("Connect mongodb");
-  app.listen(port, () => {
-    console.log("server api port " + port);
-    setInterval(function () {
-      console.log("server api port " + port);
-    }, 300000);
+// app.use("/api/user", userRoute);
+// app.use("/api/role", roleRoute);
+// app.use("/api", drinkRoute);
+// app.use("/api", categoryRoute);
+// app.use("/api", areaRoute);
+// app.use("/api", tableRoute);
+// app.use("/api", drinkOrderRoute);
+// app.use("/api", orderRoute);
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+    console.log("Connected with Mongo");
   });
+mongoose.connection.on("error", (err) => {
+  console.log("Has an error when connect with Mongo", err);
 });
 
-app.use('/api/user', userRoute);
-app.use('/api/role', roleRoute);
 
-
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(morgan('common'));
-app.get('/api', (req, res) => {
-    res.status(200).json("ok");
-});
+// app.use(bodyParser.json({limit: "50mb"}));
+// app.get('/api', (req, res) => {
+//     res.status(200).json("ok");
+// });
 // app.get('/', authorization, (req, res) => {
 //     console.log(req.user);
 //     return res.status(200).json(req.user);
 // });
 app.get('/',  (req, res) => {
-    return res.status(200).json({messger: "ok"});
+    return res.send({"message": "ok"});
 });
-const home = require("./src/routes/home");
-app.use("/home",home);
-module.exports = app;
+// const home = require("./src/routes/home");
+// app.use("/home",home);
+
+
+app.listen(port, () => {
+  console.log("server api port " + port);
+  setInterval(function () {
+    console.log("server api port " + port);
+  }, 300000);
+});
